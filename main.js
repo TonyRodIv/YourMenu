5ab68bb8466e1df9208a15e908583b31004a2b67
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-05-08 09:32:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,42 @@
 let menu = [
     {
-        title:"Buttermilk Pankakes",
-        category:"breakfast",
-        price:"15",
-        image:"./images/item-2.jpeg",
-        description:"I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
+        title: "Buttermilk Pankakes",
+        category: "breakfast",
+        price: "15",
+        image: "./images/item-2.jpeg",
+        description: "I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
     },
     {
-        title:"Buttermilk Pankakes",
-        category:"breakfast",
-        price:"12",
-        image:"./images/item-3.jpeg",
-        description:"I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
+        title: "Buttermilk Pankakes",
+        category: "breakfast",
+        price: "12",
+        image: "./images/item-3.jpeg",
+        description: "I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
     },
     {
-        title:"Buttermilk Pankakes",
-        category:"breakfast",
-        price:"19",
-        image:"./images/item-4.jpeg",
-        description:"I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
+        title: "Buttermilk Pankakes",
+        category: "breakfast",
+        price: "19",
+        image: "./images/item-4.jpeg",
+        description: "I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
     },
 ]
 let menuItens = document.getElementById('menu-itens')
-let i = 0
-function write(){
-    for(i=0;i<menu.length;i++){
 
-        menuItens.innerHTML += `
+let writeMenu = menu.map((menu) => {
+    let html = `
         <div class="item-image">
-                        <img src="${menu[i].image}" alt="" srcset="">
+                        <img src="${menu.image}" alt="${menu.title}" srcset="">
                     </div>
                     <div class="item-info">
                         <div class="item-title">
-                            <p>${menu[i].title}</p>
-                            <p class="price">R$${menu[i].price}</p>
+                            <p>${menu.title}</p>
+                            <p class="price">R$${menu.price}</p>
                         </div>
                         <div class="item-description">
-                            <p>${menu[i].description}</p>
+                            <p>${menu.description}</p>
                         </div>
                     </div>
-        `;
-    }
-
-}
-write()
+        `
+    menuItens.innerHTML += html
+});
